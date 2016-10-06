0344b9c3f86438b9d442921fed8cbdcedd131c56
Igor-Tukh <igor-tukh@yandex.ru>
2016-10-06 23:04:37 +0300

    Lab4: Prospective misstakes fixed.

--- Lab4/src/mergesort.c
+++ Lab4/src/mergesort.c
@@ -2,39 +2,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void cpy(size_t size, void*to, void*from){
+void copy_mem(size_t size, void*to, void*from){
   char* a = (char*)to;
   char* b = (char*)from;
   
-  for(int i = 0; i < size; i++) *(a + i) = *(b + i);
+  for(size_t i = 0; i < size; i++){ 
+    *(a++) = *(b++);
+  }  
 }
 
 int mergesort (void* base, size_t num, size_t size, int (*compar)(const void*,const void*)){
-  if (num > 1){
-    size_t mid = num / 2;
+  if (num <= 1) 
+    return 0;
+  
+  size_t mid = num / 2;
     
-    mergesort(base, mid, size, compar);
-    mergesort((void *)((char *)base + mid * size), num - mid, size, compar);
+  mergesort(base, mid, size, compar);
+  mergesort((void *)((char *)base + mid * size), num - mid, size, compar);
     
-    char *b = malloc(num * size);
-    char *i1 = base;
-    char *i2 = (char*)base + mid*size;
-    char *last1 = i2;
-    char *last2 = (char*)base + num*size;
-    
-    for(int i = 0; i < num; i++){
-      if (i2 >= last2 || (i1 < last1 && compar((void*)i1, (void*)i2) <= 0)){
-        cpy(size, b+i*size, i1);        
-        i1 += size;
-      }
-      else{
-        cpy(size, b+i*size, i2);
-        i2 += size;
-      }
+  char *b = malloc(num * size);
+  char *i1 = base;
+  char *i2 = (char*)base + mid*size;
+  char *last1 = i2;
+  char *last2 = (char*)base + num*size;
+   
+  for(int i = 0; i < num; i++, b += size){
+    if (i2 >= last2 || (i1 < last1 && compar((void*)i1, (void*)i2) <= 0)){
+      copy_mem(size, b, i1);        
+      i1 += size;
     }
-    
-    cpy(num * size, base, (void*)b);  
-    free(b);
+    else{
+      copy_mem(size, b, i2);
+      i2 += size;
+    }
   }
+  
+  b -= size * num;
+    
+  copy_mem(num * size, base, (void*)b);  
+  free(b);
+  
   return 0;
 }
--- Lab4/src/ms_cli.c
+++ Lab4/src/ms_cli.c
@@ -19,32 +19,44 @@ int cmpr_string(const void* a,const void* b){
 int string_to_int(char* s){
   int ans = 0;
   int sgn = 1;
-  if (*s == '-') sgn = -1;
-  for (int i = (1-sgn)/2; *(s + i) != '\0'; i++){
-    ans = ans * 10 + (int)(*(s + i))-(int)'0';
+  if (*s == '-'){ 
+    sgn = -1;
+    s++;
+  }
+  for (; *s != '\0'; s++){
+    ans = ans * 10 + (int)(*s)-(int)'0';
   }
   return ans * sgn;
 }
 int main(int argc,  char* argv[]){
   if (strcmp(argv[1], "int") == 0){
     int *a = malloc((argc - 2) * sizeof(int)); 
-    for(int i = 0; i < argc - 2; i++) *(a+i) = string_to_int(argv[i + 2]);
+    for(int i = 0; i < argc - 2; i++, a++) 
+      *a = string_to_int(argv[i + 2]);
+    a -= (argc - 2);  
     mergesort(a, argc - 2, sizeof(int), cmpr_int);
-    for(int i = 0; i < argc - 2; i++) printf("%d ", a[i]);
+    for(int i = 0; i < argc - 2; i++) 
+      printf("%d ", a[i]);
     free(a);    
   }
-  else if (strcmp(argv[1], "char") == 0){
+  else if (strcmp(argv[1], "char") == 0){ 
     char *a = malloc((argc - 2) * sizeof(char)); 
-    for(int i = 0; i < argc - 2; i++) *(a+i) = *(argv[i + 2]);
+    for(int i = 0; i < argc - 2; i++, a++) 
+      *a = *(argv[i + 2]);
+    a -= (argc - 2);  
     mergesort(a, argc - 2, sizeof(char), cmpr_char);
-    for(char* i = a; i < a + argc - 2; i++) printf("%c ", *i);
+    for(char* i = a; i < a + argc - 2; i++) 
+      printf("%c ", *i);
     free(a);    
   }
   else{
     char **a = malloc(sizeof(char*) * (argc - 2)); 
-    for(int i = 0; i < argc - 2; i++) *(a+i) = (argv[i + 2]);
+    for(int i = 0; i < argc - 2; i++, a++) 
+      *a = (argv[i + 2]);
+    a -= (argc - 2);     
     mergesort(a, argc - 2, sizeof(char*), cmpr_string);
-    for(char** i = a; i < a + argc - 2; i++) printf("%s ", *i);
+    for(char** i = a; i < a + argc - 2; i++) 
+      printf("%s ", *i);
     free(a);    
   }    
   return 0;
