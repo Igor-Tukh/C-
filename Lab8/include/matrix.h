1f70cab2f1d621af1deea7aeda69ea2e381db74d
Igor-Tukh <igor-tukh@yandex.ru>
2016-11-19 15:18:16 +0300

    Lab8: extra const added. Copy changed to memcpy

--- Lab8/include/matrix.h
+++ Lab8/include/matrix.h
@@ -9,24 +9,24 @@ public:
   Matrix(const Matrix& m);
   Matrix(std::size_t r, std::size_t c);
   
-  std::size_t get_rows();
-  std::size_t get_cols();
+  std::size_t get_rows() const;
+  std::size_t get_cols() const;
   void set(std::size_t i, std::size_t j, int val);
-  int get(std::size_t i, std::size_t j);
-  void print(FILE *f);
+  int get(std::size_t i, std::size_t j) const;
+  void print(FILE *f) const;
 
   Matrix operator=(const Matrix& m);
 
-  Matrix operator+(const Matrix& m);
-  Matrix operator-(const Matrix& m);
-  Matrix operator*(const Matrix& m);
+  Matrix operator+(const Matrix& m) const;
+  Matrix operator-(const Matrix& m) const;
+  Matrix operator*(const Matrix& m) const;
 
   Matrix& operator+=(const Matrix& m);
   Matrix& operator-=(const Matrix& m);
   Matrix& operator*=(const Matrix& m);
 
-  bool operator==(const Matrix& m);
-  bool operator!=(const Matrix& m);
+  bool operator==(const Matrix& m) const;
+  bool operator!=(const Matrix& m) const;
   
   ~Matrix();
   
--- Lab8/src/matrix.cpp
+++ Lab8/src/matrix.cpp
@@ -1,5 +1,6 @@
 #include "matrix.h"
 #include <utility>
+#include <string.h>
 
 Matrix::Matrix(const Matrix& m) {
   _rows = m._rows;
@@ -8,9 +9,7 @@ Matrix::Matrix(const Matrix& m) {
   _data = new int*[_rows];
   for(std::size_t i = 0; i < _rows; i++) {
     _data[i] = new int[_cols];
-    
-    for(std::size_t j = 0; j < _cols; j++)
-      _data[i][j] = m._data[i][j]; 
+    memcpy(_data[i], m._data[i], _cols * sizeof(int));
   }
 }
 
@@ -24,18 +23,18 @@ Matrix::Matrix(std::size_t r, std::size_t c) {
     _data[i] = new int[c]();
 }
 
-std::size_t Matrix::get_rows() { return _rows; }
-std::size_t Matrix::get_cols() { return _cols; }
+std::size_t Matrix::get_rows() const { return _rows; }
+std::size_t Matrix::get_cols() const { return _cols; }
 
 void Matrix::set(std::size_t i, std::size_t j, int val) {
   _data[i][j] = val;
 }
 
-int Matrix::get(std::size_t i, std::size_t j) {
+int Matrix::get(std::size_t i, std::size_t j) const {
   return _data[i][j];
 }
 
-void Matrix::print(FILE* f) {
+void Matrix::print(FILE* f) const {
   for(std::size_t i = 0; i < _rows; i++){ 
     for(std::size_t j = 0; j < _cols; j++)
       fprintf(f, "%d ", _data[i][j]); 
@@ -51,19 +50,19 @@ Matrix Matrix::operator=(const Matrix& m) {
   return (*this);  
 }
 
-Matrix Matrix::operator+(const Matrix& m) {
+Matrix Matrix::operator+(const Matrix& m) const {
   Matrix ans = *this;
   ans += m;
   return ans;
 }
 
-Matrix Matrix::operator-(const Matrix& m) {
+Matrix Matrix::operator-(const Matrix& m) const {
   Matrix ans = *this;
   ans -= m;
   return ans; 
 }
 
-Matrix Matrix::operator*(const Matrix& m) {
+Matrix Matrix::operator*(const Matrix& m) const {
   Matrix ans = Matrix(_rows, m._cols);
   
   for(std::size_t i = 0; i < _rows; i++)
@@ -95,7 +94,7 @@ Matrix& Matrix::operator*=(const Matrix& m) {
   return *this; 
 }
 
-bool Matrix::operator==(const Matrix& m) {
+bool Matrix::operator==(const Matrix& m) const {
   for(std::size_t i = 0; i < _rows; i++)
     for(std::size_t j = 0; j < _cols; j++)
       if (_data[i][j] != m._data[i][j])
@@ -104,7 +103,7 @@ bool Matrix::operator==(const Matrix& m) {
   return true;
 }
 
-bool Matrix::operator!=(const Matrix& m) {
+bool Matrix::operator!=(const Matrix& m) const {
   return !(*this == m);  
 }
 
